5a856d1fa670a0710167c88aa67669b981886044
Kanak Tyagi <tyagiknk.india@gmail.com>
2020-08-18 18:26:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const currTime = new Date().getHours()
+const link = 'https://google.com'
+let Greeting=''
+const cssStyle = {
+
+}
+const h1Style ={
+  color: 'Black'
+}
+
+if(currTime >=1 && currTime< 12) {
+    Greeting =" Good Morning";
+    cssStyle.color="green";
+}
+ else if (currTime >=12 && currTime <14) {
+   Greeting = " Good Afternoon";
+   cssStyle.color="Orange";
+ }
+ else if (currTime >=15 && currTime <19) {
+  Greeting = " Good Evening";
+  cssStyle.color="Red";
+} else {
+   Greeting = "Good Night";
+   cssStyle.color="Black"
+ }
+
 function App() {
-  return (
+  const [name,setName] = useState('Sir')
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+     <h1 style={h1Style}> Hi: {name}  </h1>
+      <input type="text"
+      value={name}
+      placeholder="Enter your name"
+      onChange={e => setName(e.target.value)}></input>
+      </div>
+      <span style= {cssStyle}>
+      <h2>  {Greeting}</h2>
+        </span>
       </header>
     </div>
   );
